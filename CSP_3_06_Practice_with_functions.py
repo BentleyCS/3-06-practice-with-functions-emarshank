b700c3e85d12bd2ee323e6e4ce8e59efa0eeec0c
emarshank <emarshank28@bentleyschool.org>
2025-11-10 08:36:46 -0800

    added code file

--- CSP_3_06_Practice_with_functions.py
+++ CSP_3_06_Practice_with_functions.py
@@ -7,38 +7,52 @@ def root(n):
 
 #Takes in the 3 side lengths of a triangle as arguments and returns half of
 #the perimeter of a triangle.
-def semiPerimeter():
-    pass
+def semiPerimeter(a, b, c):
+    per = a+b+c
+    return per/2
 
 #Modify the below function such that it takes in 4 arguments. multiply the first
 #argument by the difference between itself and each individual argument. Reference herons formula for more context.
-def multiplyDifferences():
-    pass
+def multiplyDifferences(d, e, f, g):
+    return d*(d-e)*(d-f)*(d-g)
 
 #Given the 3 sides of a triangle return the area.
 #use herons formula
 #Use the functions above.
-def herons():
-    pass
-
+def herons(h, i, j):
+    sp = semiPerimeter(h, i, j)
+    md = multiplyDifferences(sp, h, i, j)
+    return root(md)
 
 #quadratic equation
 
 #takes in a number as an argument and returns that number multiplied by 2.
-def denominator():
-    pass
+def denominator(a):
+    b = a*2
+    return b
 
 #Takes in two arguments. multiply the first argument by negative 1. Then
 #return the modified first argument added and subtracted by the second argument.
-def plusMinus():
-    pass
+def plusMinus(b, a):
+    c1 = (-b)+a
+    c2 = (-b)-a
+    return c1, c2
+
 #takes in three numbers as arguments. The first and third multiplied together then
 #multiplied by 4.Then subtract that result from the second argument squared.
 #Return the overall result.
-def mainCalc():
-    pass
+def mainCalc(a, b, c):
+    b2 = b**2
+    ac = a*c
+    ac4 = ac*4
+    d = b2-ac4
+    return d
 
 #The below function should take the inputs of the quadratic equation and return the result
 #Make sure to use all the formulas from this section.
-def quadratic():
-    pass
+def quadratic(a, b, c):
+    mc = mainCalc(a, b, c)
+    rt = root(abs(mc))
+    n1, n2 = plusMinus(b, rt)
+    return n1/denominator(a), n2/denominator(a)
+
